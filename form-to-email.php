1575a753848be14b6b9bb6b2f8036dd243c91547
m075 <moemotara@gmail.com>
2021-04-12 09:21:09 +0200

    formspree test

--- form-to-email.php
+++ form-to-email.php
@@ -21,7 +21,7 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'm075.dev@gmail.com';//<== update the email address
+$email_from = "$visitor_email";//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
